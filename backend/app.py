10af900755e8f06c67c4ce79e305c406e4332caa
Rwan Khalifa <rwankhalifa@Rwans-MacBook-Air.local>
2025-02-19 13:58:03 -0300

    backend

--- backend/app.py
+++ backend/app.py
@@ -236,4 +236,4 @@ for rule in app.url_map.iter_rules():
     print(rule)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
